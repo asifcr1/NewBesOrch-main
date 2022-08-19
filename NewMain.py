0b4c57da90c2581d4dac859f663dc791dcbfb092
jaafar-alaboosi <jaafar.alaboosi@coolriots.io>
2022-08-19 19:01:05 +0300

    Update APP descirption and API documentations

--- NewMain.py
+++ NewMain.py
@@ -1,5 +1,48 @@
 from fastapi import FastAPI
 from Routers import Sessions
 
-app = FastAPI()
+description = """
+BESOrchestrator is a group of well-organized and well-managed APIs to help moving Data between different data sources 
+and achieve the data centralization across all different apps!
+
+## Courses
+
+You will be having the following APIs:
+
+* **Retrieve courses details from SSG API** (_not implemented_).
+
+
+## Course Runs
+
+You will be having the following APIs:
+
+* **Synchronize courses runs across all data sources** (_not implemented_).
+
+
+## Course Sessions
+
+You will be having the following APIs:
+
+* **Retrieve course sessions from SSG API** (_implemented_).
+* **Retrieve course sessions from TadaBase API** (_implemented_).
+* **Insert course sessions to be stored in our centralized data storage in BESOrchestrator** (_implemented_).
+* **Retrieve course sessions from our centralized data storage in BESOrchestrator** (_implemented_).
+* **Insert course sessions through BESOrchestrator to SSG** (_not implemented_).
+* **Insert course sessions through BESOrchestrator to TadaBase** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "Sessions",
+        "description": "Holds the operations of course Sessions. It will take care of CRUD and Synchronizing operations\
+         for course Sessions"
+    },
+]
+
+app = FastAPI(
+    title="BESOrchestrator API",
+    description=description,
+    version="1.0.0 Beta",
+    openapi_tags=tags_metadata
+)
 app.include_router(Sessions.router)
--- Routers/Sessions.py
+++ Routers/Sessions.py
@@ -9,20 +9,51 @@ from Dao.RedisDao.RedisSessionDao import RedisSessionDao, RedisJsonSessions
 router = APIRouter()
 
 
-@router.get("/beso/courses/runs/{runId}/sessions", response_model=RedisJsonSessions, tags=['Sessions'])
+@router.get("/beso/courses/runs/{runId}/sessions", response_model=RedisJsonSessions, tags=['Sessions'],
+            summary="Retrieve course run's sessions from center storage ",
+            response_description="List of sessions retrieved")
 async def get_redis_sessions(runId: str):
+    """
+        Insert the sessions with following information:
+
+        - **run ID**: The ID of course run that contains all the sessions
+        """
     RSD = RedisSessionDao()
     CourseSessionsRedis = RSD.retrieveMember(runId)
     return CourseSessionsRedis
 
-@router.get("/SSG/courses/runs/{runId}/sessions", response_model=SSGSessionResponse, tags=['Sessions'])
+@router.get("/SSG/courses/runs/{runId}/sessions", response_model=SSGSessionResponse, tags=['Sessions'],
+            summary="Retrieve course run's sessions from SSG API ",
+            response_description="List of sessions retrieved"
+            )
 async def get_ssg_data(runId: str):
+    """
+        Insert the sessions with following information:
+
+        - **run ID**: The ID of course run that contains all the sessions
+        """
     SSD = SSGSessionDao()
     CourseSessionsSSG = SSD.retrieve(course_run_id=runId)
     return CourseSessionsSSG
 
-@router.post("/beso/courses/runs/sessions/add", response_model=SSGSessionResponse, tags=['Sessions'])
+@router.post("/beso/courses/runs/sessions/add", response_model=SSGSessionResponse, tags=['Sessions'],
+             summary="Insert course run's sessions to the center storage ",
+             response_description="List of sessions inserted"
+             )
 async def insert_ssg_to_redis(sessions: SSGSessionResponse):
+    """
+       Insert the sessions with following information:
+
+       - **run ID**: The ID of course run that contains all the sessions
+       - **Session ID**: a unique ID across sessions
+       - **modeOfTraining**: Enum value to indicate the type of training of the session
+       - **attendanceTaken**: A boolean value indicates whether there's an attendance checking for this session or not
+       - **deleted**: A boolean value indicates whether the session was deleted later or not
+       - **startDate**
+       - **endDate**
+       - **startTime**
+       - **endTime**
+       """
     SRM  = SSGToRedisMapper()
     RedisJsonSessionsObject = SRM.DataModelSessionsMapper(sessions)
     RSD = RedisSessionDao()
